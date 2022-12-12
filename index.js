a43dd51718073ad7c789595fe3003d87635efe81
rahulKTH <113025396+rahulKTH@users.noreply.github.com>
2022-12-12 18:46:18 +0530

    demo

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 import express from "express";
-import Mysql from "mysql";
 const app = express()
-import db  from "./Models/index.js"
-import Search from "./Routes/Search.route.js";
 app.use(express.json());
 
-var c = Mysql.createConnection({
-  host     : 'deardoc-staging.cjtztruxn5wp.ap-south-1.rds.amazonaws.com',
-  user     : 'admin',
-  password : 'U4UiK3kzK5t2IIdTZm7k',
-  database : 'arogya_database_staging'
+import mysql from "mysql";
+var connection = mysql.createConnection({
+  host: 'deardoc-staging.cjtztruxn5wp.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'U4UiK3kzK5t2IIdTZm7k',
+  database: 'arogya_database_staging'
+})
 
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+})
+
+app.get("/",(req,res) => {
+    connection.query('SELECT * from abha_user LIMIT 1', (err, rows) => {
+        if(err) throw err;
+        console.log('The data from users table are: \n', rows);
+        connection.end();
+    });
 });
 
-c.on('error', function(err) {
-  console.log('Not Connected'); // example : 'ER_BAD_DB_ERROR'
+app.get("/",(req,res) => {
+  connection.query('SELECT * from abha_user LIMIT 1', (err, rows) => {
+      if(err) throw err;
+      console.log('The data from users table are: \n', rows);
+      connection.end();
+  });
 });
 
-exports.connection = function() {
-  console.log('Connected');
-   // return c;
-};
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
+});
 
 app.listen(44100,'0.0.0.0')
